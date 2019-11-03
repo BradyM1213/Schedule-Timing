65e4bd5087e66276a43d2dec4039cc2687dfdb34
Henry LeCompte <lecompteh18@gmail.com>
2019-11-03 10:10:58 -0500

    AAA

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, flash, url_for
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, logout_user, LoginManager
@@ -18,15 +18,20 @@ class User(db.Model, UserMixin):
     Email = db.Column(db.String())
     Username = db.Column(db.String(), unique=True)
     PasswordH = db.Column(db.String())
+    Authenticated = db.Column(db.Boolean())
 
-    def __init__(self, Number, Email, Uname, Pass):
+    def __init__(self, Number, Email, Uname, Pass, Auth):
         self.PNumber = Number
         self.Email = Email
         self.Username = Uname
         self.PasswordH = str(hashlib.md5(Pass.encode()).hexdigest())
+        self.Authenticated = Auth
 
     def is_authenticated(self):
-        return True
+        return self.Authenticated
+
+    def get_id(self):
+        return self.id
 
 
 @login.user_loader
@@ -49,7 +54,7 @@ def Signup():
         Number = request.form["phone"]
         email = request.form["email"]
         Pass = request.form["password"]
-        user = User(Number, email, uname, Pass)
+        user = User(Number, email, uname, Pass, True)
         db.session.add(user)
         db.session.commit()
         login_user(user, remember=True)
@@ -60,13 +65,32 @@ def Signup():
 
 @app.route('/logout')
 def logout():
+    UQ = User.query.filter_by(id=User.get_id()).first()
+    UQ.Authenticated = False
+    db.session.add(UQ)
+    db.session.commit()
     logout_user()
-    return 'Logged Out'
+    flash("Logged Out")
+    return url_for('login')
 
 
 @app.route('/login')
 def login():
     return render_template("Login.html")
+
+
+@app.route('/loginuser', methods=['POST'])
+def loginuser():
+    try:
+        Pwd = request.form["password"]
+        Uname = request.form["username"]
+        UQ = User.query.filter_by(Username=Uname, Password=Pwd)
+        UQ.Authenticated = True
+        db.session.add(UQ)
+        db.session.commit()
+        login_user(UQ)
+    except Exception:
+        return 'NO'
 
 
 if __name__ == "__main__":
