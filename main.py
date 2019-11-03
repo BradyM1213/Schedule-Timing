b03c6db63eab69c419811e1a3d4f1c8645977949
Brady Markson <57106948+BradyM1213@users.noreply.github.com>
2019-11-02 23:30:32 -0400

    Ah

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-import flask
+from flask import Flask
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 
-app = flask(-__name__)
+app = Flask(__name__)
 db = SQLAlchemy(app)
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.Int(), primary_key=True)
+    id = db.Column(db.Integer(), primary_key=True)
     PNumber = db.Column(db.String())
     Email = db.Column(db.String())
     Username = db.Column(db.String())
     PasswordH = db.Column(db.String())
 
-app.route('/', methods=["GET"])
-def index:
-    return render_template("index.html")
+@app.route('/', methods=["GET"])
+def index():
+    return render_template("login.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
